9cbb4173b3b85315f9850f8090dffcd279059aa0
NightWing2110 <luuquanghoangdy@gmail.com>
2022-06-16 20:41:52 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::middleware(['auth', 'isAdmin'])->group(function (){
     Route::get('/dashboard',[FrontendController::class, 'index']);
+
+    #Admin/Category
     Route::get('categories', [CategoryController::class, 'index']);
     Route::get('add-category',[CategoryController::class, 'add']);
     Route::post('insert-category', [CategoryController::class,'insert']);
+    Route::get('edit-prod/{id}', [CategoryController::class, 'edit']);
+    Route::put('update-category/{id}', [CategoryController::class, 'update']);
 });
